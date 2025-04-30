15496661d56ee92afee9f01d9f5e8e85f81be605
Osayd_Ali <mali123@depaul.edu>
2025-04-30 13:36:15 -0500

    Removed object creation of game runner as well, just added it as component so spring can create its instance on its own

--- GameConsole/game/GameRunner.java
+++ GameConsole/game/GameRunner.java
@@ -1,5 +1,8 @@
 package GameConsole.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
     private GamingConsole game;
     public GameRunner(GamingConsole game) {
