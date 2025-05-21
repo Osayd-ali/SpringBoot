e19978e1ec5706c62c2dc428f1ecb0dc4022cebe
Mir Osayd Ali <mali123@depaul.edu>
2025-05-20 21:03:12 -0500

    Update

--- examples/a1/DepInjectionLauncherApplication.java
+++ examples/a1/DepInjectionLauncherApplication.java
@@ -1,16 +1,24 @@
-package examples.a0;
+package examples.a1;
 
-import GameConsole.game.GameRunner;
-import GameConsole.game.GamingConsole;
-import GameConsole.game.PacmanGame;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
+class YourBusinessClass{
+
+}
+@Component
+class Dependency1{
+
+}
+@Component
+class Dependency2{
+
+}
 @Configuration
 @ComponentScan
 public class DepInjectionLauncherApplication {
