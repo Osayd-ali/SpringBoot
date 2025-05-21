a2be4cd82abd613906d4ec91243d97f6e87587ae
Mir Osayd Ali <mali123@depaul.edu>
2025-05-21 01:15:26 -0500

    Field Injection Example

--- GameConsole/game/GameRunner.java
+++ GameConsole/game/GameRunner.java
@@ -9,6 +9,7 @@ public class GameRunner {
     public GameRunner(@Qualifier("SuperContraQualifier") GamingConsole game) { //Injecting the qualifier object into the constructor of the GameRunner class.
         this.game = game;
     }
+    //No, the class injected into the constructor (e.g., GamingConsole) is not dependent on the constructor's class (GameRunner) to run itself. GamingConsole is an independent interface, and its implementations (like PacmanGame) define their own behavior. GameRunner depends on GamingConsole to perform its tasks, but GamingConsole does not rely on GameRunner. This is a unidirectional dependency
     public void run(){
         System.out.println("Running game " + game);
         game.up();
--- examples/a1/DepInjectionLauncherApplication.java
+++ examples/a1/DepInjectionLauncherApplication.java
@@ -1,5 +1,7 @@
 package examples.a1;
+// Example of field injection. No setter or constructor. Dependency is injected using reflection.
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,15 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass{
+    @Autowired
+    Dependency1 dependency1; // Here we are using field injection. Spring will inject the object of this class into the YourBusinessClass class using reflection.
 
+    @Autowired
+    Dependency2 dependency2; // Here we are using field injection. Spring will inject the object of this class into the YourBusinessClass class using reflection.
+
+    public String toString(){
+        return "Using " + dependency1 + " and " + dependency2;
+    }
 }
 @Component
 class Dependency1{
@@ -25,6 +35,7 @@ public class DepInjectionLauncherApplication {
     public static void main(String[] args){
         try(var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class)) {
             Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean(YourBusinessClass.class));
         }
     }
 }
