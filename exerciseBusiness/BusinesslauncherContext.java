3b461f1e45785fe6a0eca0c5002953052933e37b
Mir Osayd Ali <mali123@depaul.edu>
2025-05-22 03:00:02 -0500

    displaying all beans

--- exerciseBusiness/BusinesslauncherContext.java
+++ exerciseBusiness/BusinesslauncherContext.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @ComponentScan
 @Configuration
 public class BusinesslauncherContext {
     public static void main(String[] args){
         try (var context = new AnnotationConfigApplicationContext(BusinesslauncherContext.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
             System.out.println(context.getBean(BusinessCalculationService.class).findMax());
         }
     }
