c3bed0c7dac73bcae1112402c643760f06e74423
Mir Osayd Ali <mali123@depaul.edu>
2025-05-23 23:28:10 -0500

    Update

--- exerciseBusiness/MongoDbDataService.java
+++ exerciseBusiness/MongoDbDataService.java
@@ -9,7 +9,7 @@ public class MongoDbDataService implements DataService {
     @Override
     public int[] retrieveData() {
         // Logic to connect to MongoDB and retrieve data
-        System.out.println("MongoDB Data Service");
+        System.out.println("MongoDB Data Service test");
         return new int[] { 11, 22, 33, 44, 55 };
     }
 }
