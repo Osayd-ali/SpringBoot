b5421e7a452dbeea5ed1cc1340f5ca61ab809ee9
Mir Osayd Ali <mali123@depaul.edu>
2025-05-24 02:29:09 -0500

    lazyInitialization

--- examples2/LazyInitializationLauncherApplication.java
+++ examples2/LazyInitializationLauncherApplication.java
@@ -4,16 +4,32 @@ package examples2;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
+class ClassA {
+
+}
+@Component
+@Lazy
+class ClassB{
+    private ClassA classA;
+    public ClassB(ClassA classA) {
+        //Logic
+        System.out.println("Some Initialization logic");
+        this.classA = classA;
+    }
+}
 
 @ComponentScan
 @Configuration
 public class LazyInitializationLauncherApplication {
     public static void main(String[] args){
         try ( var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)){
-            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            context.getBean(ClassB.class);
         }
     }
 }
