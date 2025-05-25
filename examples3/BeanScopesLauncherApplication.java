dc95a3f8883eeeccf1b0ad73c226aaa805e60368
Mir Osayd Ali <mali123@depaul.edu>
2025-05-24 22:56:32 -0500

    PrototypeandSingleton

---
+++ examples3/BeanScopesLauncherApplication.java
@@ -0,0 +1,40 @@
+// Exploring Java Spring Framework Bean Scopes
+package examples3;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+@Component
+class NormalClass {
+    public NormalClass() { //Everytime this bean is requested, the same instance is returned.
+        System.out.println("NormalClass Constructor");
+    }
+}
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass { //Everytime this bean is requested, a new instance is created.
+    public PrototypeClass() { // By default, the scope of a bean is singleton.
+        System.out.println("PrototypeClass Constructor");
+    }
+}
+@Configuration
+@ComponentScan
+public class BeanScopesLauncherApplication {
+    public static void main(String[] args){
+        try(var context = new AnnotationConfigApplicationContext(BeanScopesLauncherApplication.class)){
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+        }
+    }
+}
