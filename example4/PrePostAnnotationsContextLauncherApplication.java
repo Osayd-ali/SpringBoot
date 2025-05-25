2a95eae67669d695fcdf14e3ef9f251e2c80d4a0
Mir Osayd Ali <mali123@depaul.edu>
2025-05-25 00:25:40 -0500

    PreConstructandPostDestroy

--- example4/PrePostAnnotationsContextLauncherApplication.java
+++ example4/PrePostAnnotationsContextLauncherApplication.java
@@ -1,6 +1,7 @@
 // Exploring Spring beans, PostConstruct and PreDestroy annotations
 package example4;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,20 @@ class SomeClass {
         this.someDependency = someDependency;
         System.out.println("All dependencies are ready");
     }
+    //As soon as the dependency is injected, this method will be called.
+    // How can i get the below method to run, as soon as the dependencies are wired in?
+    // @PostConstruct, we can use this annotation to run the method after the dependencies are injected.
+    @PostConstruct
+    public void initialize(){
+        someDependency.getReady();
+    }
 }
 
 @Component
 class SomeDependency{
-
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency");
+    }
 }
 @Configuration
 @ComponentScan
