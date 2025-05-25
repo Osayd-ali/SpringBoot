83aa39d01b4319406915f65c82ce7c3f4e15f1dc
Mir Osayd Ali <mali123@depaul.edu>
2025-05-25 00:04:39 -0500

    PreConstructandPostDestroy

--- example4/PrePostAnnotationsContextLauncherApplication.java
+++ example4/PrePostAnnotationsContextLauncherApplication.java
@@ -2,9 +2,13 @@
 package example4;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 
+@Configuration
+@ComponentScan
 public class PrePostAnnotationsContextLauncherApplication {
     public static void main(String[] args){
         try(var context = new AnnotationConfigApplicationContext(PrePostAnnotationsContextLauncherApplication.class)){
