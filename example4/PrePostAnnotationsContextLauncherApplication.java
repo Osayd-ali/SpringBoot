7d92979c90e28dd86859c44611db200289dbad7b
Mir Osayd Ali <mali123@depaul.edu>
2025-05-25 00:13:26 -0500

    PreConstructandPostDestroy

--- example4/PrePostAnnotationsContextLauncherApplication.java
+++ example4/PrePostAnnotationsContextLauncherApplication.java
@@ -4,9 +4,23 @@ package example4;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
+class SomeClass {
+    private SomeDependency someDependency;
+    public SomeClass (SomeDependency someDependency) {
+        System.out.println("Constructor Injection");
+        this.someDependency = someDependency;
+    }
+}
+
+@Component
+class SomeDependency{
+
+}
 @Configuration
 @ComponentScan
 public class PrePostAnnotationsContextLauncherApplication {
