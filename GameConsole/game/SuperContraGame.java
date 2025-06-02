d9c9236720c3f5206fdcb96e1198f4041a8a94e6
Mir Osayd Ali <mali123@depaul.edu>
2025-06-01 19:25:23 -0500

    refactored

--- GameConsole/game/SuperContraGame.java
+++ GameConsole/game/SuperContraGame.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("SuperContraQualifier") //Telling spring to create an instance of this specific class with the name "SuperContraQualifier".)
+@Qualifier("SuperContraQualifier:") //Telling spring to create an instance of this specific class with the name "SuperContraQualifier".)
 public class SuperContraGame implements GamingConsole{
     public void up(){
         System.out.println("Up");
