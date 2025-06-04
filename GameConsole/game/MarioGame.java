8dfece735cabdfc53cd87668596d4a3420af093a
Mir Osayd Ali <mali123@depaul.edu>
2025-06-03 21:52:11 -0500

    refactored

--- GameConsole/game/MarioGame.java
+++ GameConsole/game/MarioGame.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Primary //Telling spring to create this object as the primary object when there are multiple objects of the same type.
 public class MarioGame implements GamingConsole{
     public void up(){
-        System.out.println("Jump");
+        System.out.println("Jump:");
     }
     public void left(){
         System.out.println("Go back");
